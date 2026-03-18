bf0f4bf680f7c2c8cb4a6b1b575a05f130bb3f18
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-18 20:41:39 +0530

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class TaskCreate(BaseModel):
+    title: str
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    completed: bool
+
+    class Config:
+        orm_mode = True
